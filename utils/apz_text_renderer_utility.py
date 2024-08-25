93ab79ee50e183f8ae677273d556716dc7785c73
Pablo APZmedia <me@apzmedia.com>
2024-08-26 01:42:02 +0200

    fonts

--- utils/apz_text_renderer_utility.py
+++ utils/apz_text_renderer_utility.py
@@ -1,17 +1,12 @@
-# apz_text_renderer_utility.py
-
-from PIL import ImageDraw
-from ..utils.apz_text_box_utility import TextBoxUtility
-from ..utils.apz_color_utility import ColorUtility
-
 class TextRendererUtility:
     @staticmethod
     def render_text(draw, wrapped_lines, box_start_x, box_start_y, padding, effective_textbox_width, effective_textbox_height, font_manager, color_utility, alignment, vertical_alignment, line_height_ratio, font_color_rgb, italic_font_color_rgb, bold_font_color_rgb):
-        # Assuming wrapped_lines contain the correct font size information
         font_size = wrapped_lines[0][1][0][1]['size'] if wrapped_lines else 0
-        y = TextBoxUtility.calculate_initial_y(vertical_alignment, box_start_y, padding, effective_textbox_height, len(wrapped_lines) * int(font_size * line_height_ratio))
         for line, line_parts in wrapped_lines:
-            x = TextBoxUtility.calculate_initial_x(alignment, box_start_x, padding, effective_textbox_width, line, font_manager.get_default_font())
+            # Assume the first font used in line_parts as the default font for alignment calculations
+            default_font = font_manager.get_font_for_style(line_parts[0][1])
+            x = TextBoxUtility.calculate_initial_x(alignment, box_start_x, padding, effective_textbox_width, line, default_font)
+            y = TextBoxUtility.calculate_initial_y(vertical_alignment, box_start_y, padding, effective_textbox_height, len(wrapped_lines) * int(font_size * line_height_ratio))
             for chunk, chunk_styles in line_parts:
                 current_font = font_manager.get_font_for_style(chunk_styles)
                 current_font_color_rgb = color_utility.get_font_color(chunk_styles, font_color_rgb, italic_font_color_rgb, bold_font_color_rgb)
