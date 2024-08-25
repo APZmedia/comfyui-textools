cacde6f6057174936e5a15192f9f3bb05c3d3c4a
Pablo APZmedia <me@apzmedia.com>
2024-08-26 01:38:03 +0200

    refactoring

--- utils/apz_font_loader_utility.py
+++ utils/apz_font_loader_utility.py
@@ -15,6 +15,12 @@ class FontLoaderUtility:
             line_height = int(font_size * line_height_ratio)
             parsed_text = parse_rich_text(theText)
             wrapped_lines, total_text_height = wrap_text(parsed_text, loaded_font, effective_textbox_width, line_height)
+
+            # Attach font size to the style dictionary in wrapped_lines
+            for line, line_parts in wrapped_lines:
+                for chunk, chunk_styles in line_parts:
+                    chunk_styles['size'] = font_size
+
             if total_text_height <= effective_textbox_height:
                 return font_size, wrapped_lines, total_text_height
             font_size -= 1
--- utils/apz_text_renderer_utility.py
+++ utils/apz_text_renderer_utility.py
@@ -7,7 +7,9 @@ from ..utils.apz_color_utility import ColorUtility
 class TextRendererUtility:
     @staticmethod
     def render_text(draw, wrapped_lines, box_start_x, box_start_y, padding, effective_textbox_width, effective_textbox_height, font_manager, color_utility, alignment, vertical_alignment, line_height_ratio, font_color_rgb, italic_font_color_rgb, bold_font_color_rgb):
-        y = TextBoxUtility.calculate_initial_y(vertical_alignment, box_start_y, padding, effective_textbox_height, len(wrapped_lines) * int(font_manager.get_font_size() * line_height_ratio))
+        # Assuming wrapped_lines contain the correct font size information
+        font_size = wrapped_lines[0][1][0][1]['size'] if wrapped_lines else 0
+        y = TextBoxUtility.calculate_initial_y(vertical_alignment, box_start_y, padding, effective_textbox_height, len(wrapped_lines) * int(font_size * line_height_ratio))
         for line, line_parts in wrapped_lines:
             x = TextBoxUtility.calculate_initial_x(alignment, box_start_x, padding, effective_textbox_width, line, font_manager.get_default_font())
             for chunk, chunk_styles in line_parts:
@@ -24,4 +26,4 @@ class TextRendererUtility:
                     draw.line((x, strikeout_y, x + chunk_width, strikeout_y), fill=current_font_color_rgb, width=1)
 
                 x += chunk_width
-            y += int(font_manager.get_font_size() * line_height_ratio)
+            y += int(font_size * line_height_ratio)
