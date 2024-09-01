42929ec030f20fd4edd7f5782383bb00e644e26b
Pablo APZmedia <me@apzmedia.com>
2024-09-01 14:44:20 +0200

    Debugging font manager

--- utils/apz_font_manager.py
+++ utils/apz_font_manager.py
@@ -8,15 +8,24 @@ class FontManager:
         self.bold_font_path = bold_font_path
         self.max_font_size = max_font_size
 
+        # Print statements to confirm paths
+        print(f"Initialized FontManager with Regular Font: {regular_font_path}")
+        print(f"Italic Font: {italic_font_path}")
+        print(f"Bold Font: {bold_font_path}")
+
         # Dictionary to cache loaded fonts
         self.font_cache = {}
+
+        
 
     def load_font(self, font_path, font_size):
         # Load font from cache if available
         if (font_path, font_size) not in self.font_cache:
+            print(f"Loading font from path: {font_path} with size: {font_size}")
             font = ImageFont.truetype(font_path, font_size)
             self.font_cache[(font_path, font_size)] = font
         return self.font_cache[(font_path, font_size)]
+    
 
     def get_regular_font(self, font_size):
         return self.load_font(self.regular_font_path, font_size)
@@ -29,8 +38,11 @@ class FontManager:
 
     def get_font_for_style(self, style, font_size):
         if 'b' in style:
+            print(f"Selected Bold Font for style '{style}' with size: {font_size}")
             return self.get_bold_font(font_size)
         elif 'i' in style:
+            print(f"Selected Bold Font for style '{style}' with size: {font_size}")
             return self.get_italic_font(font_size)
         else:
+            print(f"Selected Bold Font for style '{style}' with size: {font_size}")
             return self.get_regular_font(font_size)
